6acf21a5d30a690f48abec9eacd18c1eb72b2495
datamate-lanka <datamate.lanka@gmail.com>
2024-12-09 19:10:37 +0530

    Update subz.js

--- plugins/subz.js
+++ plugins/subz.js
@@ -2,7 +2,7 @@ const config = require('../config');
 const { cmd, commands } = require('../command');
 const { getSubz } = require("subz.lk");
 const axios = require('axios');
-const fs = require('fs');
+const fs = require('fs-extra');
 
 cmd({
     pattern: "subtitle",
